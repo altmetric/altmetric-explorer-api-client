ffe48336b746760e386608fb04f959754de727c7
Adrian Mowat <adrian@altmetric.com>
2024-06-19 11:04:47 +0100

    chore: regenerate python version file on startup

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env sh
 
+echo "INFO : Syncing .python-version with the container"
+printf '%s\n' $PYTHON_VERSION > .python-version
+
 echo "INFO : Starting a daemon to keep the container running"
 tail -f /dev/null
