003cbd96f096edd8a53bbc5c571a9152e9e2dbcf
Adrian Mowat <adrian@altmetric.com>
2024-06-24 16:44:05 +0100

    doc: add and clean up pydoc comments

--- altmetric/explorer/api/filters.py
+++ altmetric/explorer/api/filters.py
@@ -1,17 +1,41 @@
 class Filters:
-    '''Holds a list of filters and provides accessors to the authentication message string as well
-       as the query string itself.'''
+    '''Holds a list of filters and provides methods that transform them
+     into query parameters and message digests.'''
+
     def __init__(self):
+        """Initializes a new Filters object"""
         self.items = []
 
     def add_filter(self, arg, value):
+        """Add a new filter to the list of filters
+
+        Args:
+            arg (string): name of the filter
+            value (string, list, tuple or set): filter value(s)
+
+        Returns:
+            Filters: self
+        """
         self.items.append((arg, value))
         return self
 
     def __len__(self):
+        """Count of filters
+
+        Returns:
+            integer: number of filters stored
+        """
         return len(self.items)
 
     def message(self):
+        """Convert the Filters to a message that can be used to construct a
+        digest for authentication.
+
+        See https://www.altmetric.com/explorer/documentation/api#authentication
+
+        Returns:
+            string: the digest
+        """
         result = []
         for arg, value in sorted(self.items):
             result.append(arg)
@@ -23,6 +47,12 @@ class Filters:
         return '|'.join(result)
 
     def __str__(self):
+        """Create a string representation of the Filters in the form of query
+        parameters for convenience.
+
+        Returns:
+            string: api query parameters
+        """
         result = []
         for arg, value in self.items:
             if type(value) == list or type(value) == tuple:
@@ -31,5 +61,3 @@ class Filters:
             else:
                 result.append(f'filter[{arg}]={value}')
         return '&'.join(result)
-
-
