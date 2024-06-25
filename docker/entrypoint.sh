6ec936a14bb448409943530f70cb7cd72061640a
Adrian Mowat <adrian@altmetric.com>
2024-06-25 17:21:15 +0100

    doc: re-write the querying example as a Jupyter notebook

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,5 +3,5 @@
 echo "INFO : Syncing .python-version with the container"
 printf '%s\n' $PYTHON_VERSION > .python-version
 
-echo "INFO : Starting a daemon to keep the container running"
-tail -f /dev/null
+echo "INFO : Starting jupyter lab at http://localhost:8888"
+jupyter lab --allow-root --no-browser --ip 0.0.0.0 --LabApp.token=''
